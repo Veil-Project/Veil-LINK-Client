0ecaf57817f4e89604c9770fd412290b82785af2
Alex Bendiken <alex@bndk.co>
2020-04-10 09:24:52 +1200

    Refactor unspendable balance block

--- src/components/Sidebar/UnspendableBalanceBlock.tsx
+++ src/components/Sidebar/UnspendableBalanceBlock.tsx
@@ -3,6 +3,25 @@ import { useStore } from 'store'
 import { Link } from '@reach/router'
 import SidebarBlock from './SidebarBlock'
 
+interface RowProps {
+  label: string
+  amount: number
+}
+
+const Row = ({ label, amount }: RowProps) => (
+  <div className="flex justify-between items-baseline">
+    <span className="font-medium">{label}</span>
+    <span className="h-px bg-gray-600 flex-1 mx-2" />
+    <span className="text-teal-500 font-semibold">
+      {amount.toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      })}{' '}
+      VEIL
+    </span>
+  </div>
+)
+
 const UnspendableBalanceBlock = () => {
   const { state } = useStore()
   const { balance } = state
@@ -10,45 +29,16 @@ const UnspendableBalanceBlock = () => {
   return (
     <SidebarBlock title="Unspendable Balances">
       <div className="leading-relaxed text-sm">
-        {balance.unconfirmedBalance !== null && balance.unconfirmedBalance > 0 && (
-          <div className="flex justify-between items-baseline font-medium">
-            <span className="">Unconfirmed</span>
-            <span className="h-px bg-gray-600 flex-1 mx-2" />
-            <span className="text-teal-500">
-              {balance.unconfirmedBalance.toLocaleString('en-US', {
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2,
-              })}{' '}
-              VEIL
-            </span>
-          </div>
-        )}
+        {balance.unconfirmedBalance !== null &&
+          balance.unconfirmedBalance > 0 && (
+            <Row label="Unconfirmed" amount={balance.unconfirmedBalance} />
+          )}
         {balance.immatureBalance !== null && balance.immatureBalance > 0 && (
-          <div className="flex justify-between items-baseline font-medium">
-            <span className="">Immature</span>
-            <span className="h-px bg-gray-600 flex-1 mx-2" />
-            <span className="text-teal-500">
-              {balance.immatureBalance.toLocaleString('en-US', {
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2,
-              })}{' '}
-              VEIL
-            </span>
-          </div>
+          <Row label="Immature" amount={balance.immatureBalance} />
         )}
         {balance.legacyBalance !== null && balance.legacyBalance > 0 && (
           <>
-            <div className="flex justify-between items-baseline font-medium">
-              <span className="">Legacy</span>
-              <span className="h-px bg-gray-600 flex-1 mx-2" />
-              <span className="text-teal-500">
-                {balance.legacyBalance.toLocaleString('en-US', {
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 2,
-                })}{' '}
-                VEIL
-              </span>
-            </div>
+            <Row label="Legacy" amount={balance.legacyBalance} />
             <div className="text-xs mb-1">
               <Link
                 to="/convert"
