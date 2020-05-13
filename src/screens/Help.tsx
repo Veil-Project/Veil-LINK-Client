e553057ca9941f1ac0239d6d1dc130c542da67f9
Alex Bendiken <alex@bndk.co>
2020-05-13 21:43:57 +1200

    Hide reveal files buttons from help when connected via RPC

--- src/screens/Help.tsx
+++ src/screens/Help.tsx
@@ -24,14 +24,14 @@ const Help = () => {
 
   return (
     <Modal className="p-10" canClose={true}>
-      <ul className="leading-loose grid grid-cols-2 gap-2 text-center">
+      <ul className="grid grid-cols-2 gap-2 leading-loose text-center">
         <li className="flex w-48">
           <ExternalLink
             href="https://veil-project.com"
-            className="w-full p-4 rounded text-gray-300 hover:text-white"
+            className="w-full p-4 text-gray-300 rounded hover:text-white"
             style={{ backgroundColor: 'rgba(255, 255, 255, 0.05)' }}
           >
-            <div className="h-24 flex items-center justify-center">
+            <div className="flex items-center justify-center h-24">
               <VeilLogo className="w-20 h-20 mx-auto" />
             </div>
             Veil Website
@@ -40,10 +40,10 @@ const Help = () => {
         <li className="flex w-48">
           <ExternalLink
             href="https://veil.freshdesk.com/support/home"
-            className="w-full p-4 rounded text-gray-300 hover:text-white"
+            className="w-full p-4 text-gray-300 rounded hover:text-white"
             style={{ backgroundColor: 'rgba(255, 255, 255, 0.05)' }}
           >
-            <div className="h-24 flex items-center justify-center">
+            <div className="flex items-center justify-center h-24">
               <svg
                 height="72px"
                 width="72px"
@@ -83,18 +83,18 @@ const Help = () => {
         <li className="flex w-48">
           <ExternalLink
             href="https://discord.veil-project.com"
-            className="w-full p-4 rounded text-gray-300 hover:text-white"
+            className="w-full p-4 text-gray-300 rounded hover:text-white"
             style={{ backgroundColor: 'rgba(255, 255, 255, 0.05)' }}
           >
-            <div className="h-24 flex items-center justify-center">
-              <div className="p-5 pt-6 rounded-full flex items-center justify-center bg-blue-500">
+            <div className="flex items-center justify-center h-24">
+              <div className="flex items-center justify-center p-5 pt-6 bg-blue-500 rounded-full">
                 <svg
                   enableBackground="new 0 0 24 24"
                   height="40px"
                   width="40px"
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
-                  className="fill-current -mb-1"
+                  className="-mb-1 fill-current"
                 >
                   <g className="text-teal-500">
                     <path d="m3.58 21.196h14.259l-.681-2.205c.101.088 5.842 5.009 5.842 5.009v-21.525c-.068-1.338-1.22-2.475-2.648-2.475l-16.767.003c-1.427 0-2.585 1.139-2.585 2.477v16.24c0 1.411 1.156 2.476 2.58 2.476zm10.548-15.513-.033.012.012-.012zm-7.631 1.269c1.833-1.334 3.532-1.27 3.532-1.27l.137.135c-2.243.535-3.26 1.537-3.26 1.537.104-.022 4.633-2.635 10.121.066 0 0-1.019-.937-3.124-1.537l.186-.183c.291.001 1.831.055 3.479 1.26 0 0 1.844 3.15 1.844 7.02-.061-.074-1.144 1.666-3.931 1.726 0 0-.472-.534-.808-1 1.63-.468 2.24-1.404 2.24-1.404-3.173 1.998-5.954 1.686-9.281.336-.031 0-.045-.014-.061-.03v-.006c-.016-.015-.03-.03-.061-.03h-.06c-.204-.134-.34-.2-.34-.2s.609.936 2.174 1.404c-.411.469-.818 1.002-.818 1.002-2.786-.066-3.802-1.806-3.802-1.806 0-3.876 1.833-7.02 1.833-7.02z" />
@@ -110,10 +110,10 @@ const Help = () => {
         <li className="flex w-48">
           <ExternalLink
             href="https://explorer.veil-project.com"
-            className="w-full p-4 rounded text-gray-300 hover:text-white"
+            className="w-full p-4 text-gray-300 rounded hover:text-white"
             style={{ backgroundColor: 'rgba(255, 255, 255, 0.05)' }}
           >
-            <div className="h-24 flex items-center justify-center">
+            <div className="flex items-center justify-center h-24">
               <svg
                 id="Layer_3"
                 enableBackground="new 0 0 64 64"
@@ -167,20 +167,22 @@ const Help = () => {
           </ExternalLink>
         </li>
       </ul>
-      <div className="text-center flex flex-col items-center mt-6 -mb-2 text-sm leading-loose font-medium">
-        <button
-          className="underline text-gray-300 hover:text-white hover:no-underline"
-          onClick={revealConfig}
-        >
-          Reveal configuration file
-        </button>
-        <button
-          className="underline text-gray-300 hover:text-white hover:no-underline"
-          onClick={revealDebugLog}
-        >
-          Reveal debug log
-        </button>
-      </div>
+      {state.app.connectionMethod !== 'rpc' && (
+        <div className="flex flex-col items-center mt-6 -mb-2 text-sm font-medium leading-loose text-center">
+          <button
+            className="text-gray-300 underline hover:text-white hover:no-underline"
+            onClick={revealConfig}
+          >
+            Reveal configuration file
+          </button>
+          <button
+            className="text-gray-300 underline hover:text-white hover:no-underline"
+            onClick={revealDebugLog}
+          >
+            Reveal debug log
+          </button>
+        </div>
+      )}
     </Modal>
   )
 }
