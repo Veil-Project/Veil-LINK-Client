783b781fe3f5f25ffe1269b292c9996a0ffee172
Alex Bendiken <alex@bndk.co>
2020-05-13 16:49:28 +1200

    Ensure daemon stop resolves if it hasn't been started at all

--- electron/Daemon.ts
+++ electron/Daemon.ts
@@ -286,7 +286,7 @@ export default class Daemon extends EventEmitter {
       return Promise.reject()
     }
 
-    if (this.status === 'stopped') {
+    if (this.status === 'stopped' || !this.isRunning) {
       return Promise.resolve(this.status)
     }
 
